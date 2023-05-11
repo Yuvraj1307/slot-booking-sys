96cd5e48f9faaf53200e15dd80577ee9272f1e8f
Yuvraj Maharshi <yogeshnda2018@gmail.com.com>
2023-05-11 20:46:54 +0530

    test3

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-require('dotenv').config();
+import * as dotenv from 'dotenv'  
+dotenv.config()
 
 const connection = mongoose.connect(`${process.env.MongoURL}`);
 
